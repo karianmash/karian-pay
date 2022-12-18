86810226b18dcc32b245ccf2d9ae98a3ab990f7d
Ian Macharia <ianmachariak17@gmail.com>
2022-12-18 16:54:07 +0300

    Update mpesa-api.model.ts

--- src/models/mpesa-api.model.ts
+++ src/models/mpesa-api.model.ts
@@ -3,7 +3,7 @@ import { Buffer } from "buffer";
 import axios from "axios";
 import { getCurrentTimeStamp } from "../utils";
 import { IPaymentItem } from "../interface";
-111
+11
 class MpesaApi {
   // mpesaApi: any;
   consumerKey = process.env.MPESA_CONSUMER_KEY ?? "";
